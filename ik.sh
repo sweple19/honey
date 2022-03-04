08541619d7edb1d0d5e42d6762518633627e95e2
sweple19 <100939338+sweple19@users.noreply.github.com>
2022-03-04 19:17:21 +0700

    Update ik.sh

--- ik.sh
+++ ik.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run --name honey -d --restart always honeygain/honeygain -tou-accept -email sankyu@didncego.ru -pass opakgoreng -device testing
 docker run -d --restart always -e "P2P_EMAIL" --name peer2profit peer2profit/peer2profit_x86_64:latest
-echo "@reboot sleep 1800 && systemctl start docker\n0 */19 * * * sudo reboot" > testcron;crontab testcron;rm -f testcron
+printf "@reboot sleep 1800 && systemctl start docker\n0 */19 * * * sudo reboot" > testcron;crontab testcron;rm -f testcron
